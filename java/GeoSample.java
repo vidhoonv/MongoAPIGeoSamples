8db156e5aae4388d9f87a1154f1b13583546b95b
Vishi Viswanathan <viviswan@microsoft.com>
2017-03-17 16:24:38 -0700

    Add sample for Java

---
+++ java/GeoSample.java
@@ -0,0 +1,96 @@
+package mongogeotest;
+
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.ReadPreference;
+import com.mongodb.Tag;
+import com.mongodb.TagSet;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+
+public class GeoSample {
+	
+   public static void main(String[] args)
+   {
+	   try 
+	   {	   
+		   
+		   MongoClientURI uri = new MongoClientURI("mongodb://<username>:<password>@<DocDBAccountEndpoint>:<port>/<dbname>?ssl=true&replicaSet=globaldb");
+		   MongoClient mongoClient = new MongoClient(uri);
+		   
+		   MongoDatabase database = mongoClient.getDatabase("<dbname>");
+		   
+		   //Example of inserting docs (goes to write region)
+		   MongoCollection<Document> collection = database.getCollection("<collname>");
+		   List<Document> docs = new ArrayList<Document>();
+		   docs.add(new Document("fruit", "apple"));
+		   docs.add(new Document("fruit", "orange"));
+		   docs.add(new Document("fruit", "mango"));
+		   collection.insertMany(docs);
+		   
+		   //Example of reading from Write region (readpreference = primary)
+		   MongoCollection<Document> writeRegionCollection = database.getCollection("<collname>").withReadPreference(ReadPreference.primary());
+		   List<Document> foundDocument = writeRegionCollection.find().into(new ArrayList<Document>());
+		   System.out.println("Documents from write region:");
+		   for(Iterator<Document> idoc = foundDocument.iterator(); idoc.hasNext();)
+		   {
+			    Document item = idoc.next();
+			    System.out.println(item);
+		   }
+		   
+		   //Example of reading from Nearest region (readpreference = nearest)
+		   MongoCollection<Document> nearestRegionCollection = database.getCollection("<collname>").withReadPreference(ReadPreference.nearest());
+		   foundDocument = nearestRegionCollection.find().into(new ArrayList<Document>());
+		   System.out.println("Documents from nearest region:");
+		   for(Iterator<Document> idoc = foundDocument.iterator(); idoc.hasNext();)
+		   {
+			    Document item = idoc.next();
+			    System.out.println(item);
+		   }	   
+
+		   
+		   //Example of reading from Read region (readpreference = secondary)
+		   MongoCollection<Document> readRegionCollection = database.getCollection("<collname>").withReadPreference(ReadPreference.secondary());
+		   foundDocument = readRegionCollection.find().into(new ArrayList<Document>());
+		   System.out.println("Documents from read region:");
+		   for(Iterator<Document> idoc = foundDocument.iterator(); idoc.hasNext();)
+		   {
+			    Document item = idoc.next();
+			    System.out.println(item);
+		   }
+		   
+		   //Example of reading from a specific region using tags
+		   Tag tag = new Tag("region", "<regionName>"); //you specify your read region here as a Tag.
+		   TagSet tagset = new TagSet(tag);
+		   List<TagSet> tagsetList = new ArrayList<TagSet>();
+		   tagsetList.add(tagset);
+		   
+		   ReadPreference regionPref = ReadPreference.nearest(tagsetList);
+		   MongoCollection<Document> specificRegionCollection = database.getCollection("<collname>").withReadPreference(regionPref);
+		   foundDocument = specificRegionCollection.find().into(new ArrayList<Document>());
+		   System.out.println("Documents from specific region :");
+		   for(Iterator<Document> idoc = foundDocument.iterator(); idoc.hasNext();)
+		   {
+			    Document item = idoc.next();
+			    System.out.println(item);
+		   }
+		   
+		   
+		   System.out.println("Sample complete");
+		   
+	   } 
+	   catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+   }
+}
